9cf8a5255278a370b2509f1f86fd9f985c0984e4
SVALLA <svalla@cross-systems.ch>
2014-05-06 16:40:59 +0200

    test

--- webapp/src/test/java/test/unit/org/testinfected/petstore/views/CheckoutPageTest.java
+++ webapp/src/test/java/test/unit/org/testinfected/petstore/views/CheckoutPageTest.java
@@ -99,7 +99,7 @@ public class CheckoutPageTest {
 
         checkoutPage = renderCheckoutPage().with(checkout.withPayment(paymentDetails)).asDom();
 
-        assertThat("billing information", checkoutPage, hasCheckoutForm(hasBillingInformation("Jack", "Johnson", "jack@gmail.com")));
+        assertThat("billing information", checkoutPage, hasCheckoutForm(hasBillingInformation("Jack", "Johnson", "jack@gmail.com", "", "")));
         //assertThat("billing form has no street and city fields", checkoutPage, hasCheckoutForm(hasStreetAndCityBillingInformation("A street", "A City")));
         assertThat("payment information", checkoutPage, hasCheckoutForm(hasCreditCardDetails(CreditCardType.visa, "4111111111111111", "2015-10-10")));
     }
@@ -109,23 +109,20 @@ public class CheckoutPageTest {
     }
 
     private Matcher<Element> hasEmptyBillingInformation() {
-        return hasBillingInformation("", "", "");
+        return hasBillingInformation("", "", "", "", "");
     }
 
     @SuppressWarnings("unchecked")
-    private Matcher<Element> hasBillingInformation(String firstName, String lastName, String email) {
-        return hasUniqueSelector("#billing-address", hasInputFields(matches(
+    private Matcher<Element> hasBillingInformation(String firstName, String lastName, String email, String street, String city) {
+        return hasUniqueSelector("#billing-address",hasInputFields(matches(
                 anElement(hasName("first-name"), hasAttribute("value", firstName)),
                 anElement(hasName("last-name"), hasAttribute("value", lastName)),
-                anElement(hasName("email"), hasAttribute("value", email)))));
+                anElement(hasName("email"), hasAttribute("value", email)),
+                anElement(hasName("street"), hasAttribute("value", street)),
+                anElement(hasName("city"), hasAttribute("value", city))
+                )));
     }
     
-    @SuppressWarnings("unchecked")
-    private Matcher<Element> hasStreetAndCityBillingInformation(String street, String city) {
-        return hasUniqueSelector("#billing-address", hasInputFields(matches(
-                anElement(hasName("street"), hasAttribute("value", street)),
-                anElement(hasName("city"), hasAttribute("value", city)))));
-    }
 
     @SuppressWarnings("unchecked")
     private Matcher<Element> hasEmptyPaymentDetails() {

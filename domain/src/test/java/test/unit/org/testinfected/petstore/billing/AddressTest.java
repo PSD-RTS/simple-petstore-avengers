57428758910b59f1ac209154c0c302271894384a
Cao Tri Do <caotri.do74@gmail.com>
2014-05-07 13:21:14 +0200

    add sql file

--- domain/src/test/java/test/unit/org/testinfected/petstore/billing/AddressTest.java
+++ domain/src/test/java/test/unit/org/testinfected/petstore/billing/AddressTest.java
@@ -21,10 +21,10 @@ public class AddressTest {
     @Test public void
     addressesMatchWhenAllPropertiesMatch() {
     	AddressBuilder address = anAddress().
-                withFirstName("Jack").
-                withLastName("Johnson").
-                withEmail("jack@gmail.com")
-                .withStreet("rue de la rue").
+    			withFirstName("John").
+                withLastName("Doe").
+                withEmail("jdoe@gmail.com").
+                withStreet("rue de la rue").
                 withCity("Geneve").
                 withZipcode("1203").
                 withState("Geneve").
@@ -32,8 +32,8 @@ public class AddressTest {
     	AddressBuilder shouldMatch = anAddress().
                 withFirstName("John").
                 withLastName("Doe").
-                withEmail("jdoe@gmail.com")
-                .withStreet("rue de la rue").
+                withEmail("jdoe@gmail.com").
+                withStreet("rue de la rue").
                 withCity("Geneve").
                 withZipcode("1203").
                 withState("Geneve").
@@ -41,8 +41,8 @@ public class AddressTest {
     	AddressBuilder shouldNotMatch = anAddress().
                 withFirstName("Jane").
                 withLastName("Doe").
-                withEmail("jdoe@gmail.com")
-                .withStreet("rue de la rue").
+                withEmail("jdoe@gmail.com").
+                withStreet("rue de la rue").
                 withCity("Geneve").
                 withZipcode("1203").
                 withState("Geneve").
